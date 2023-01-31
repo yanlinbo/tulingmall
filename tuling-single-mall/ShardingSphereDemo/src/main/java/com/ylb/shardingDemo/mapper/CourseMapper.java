ad8c862b3cb87c3a8d28ee90e861ca45fcec279b
yanlinbo <973664322@qq.com>
2023-02-01 00:26:40 +0800

    20230201 严林博 新增分库分表使用demo

---
+++ tuling-single-mall/ShardingSphereDemo/src/main/java/com/ylb/shardingDemo/mapper/CourseMapper.java
@@ -0,0 +1,13 @@
+package com.ylb.shardingDemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ylb.shardingDemo.entity.Course;
+
+/**
+ * @author ：楼兰
+ * @date ：Created in 2021/1/4
+ * @description:
+ **/
+
+public interface CourseMapper extends BaseMapper<Course> {
+}
---
+++ tuling-single-mall/ShardingSphereDemo/src/main/java/com/ylb/shardingDemo/mapper/DictMapper.java
@@ -0,0 +1,12 @@
+package com.ylb.shardingDemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ylb.shardingDemo.entity.Dict;
+
+/**
+ * @author ：楼兰
+ * @date ：Created in 2021/1/5
+ * @description:
+ **/
+public interface DictMapper extends BaseMapper<Dict> {
+}
