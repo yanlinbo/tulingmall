4f4e93189d12d10e33c21903cf3789f4af098a76
yanlinbo <973664322@qq.com>
2023-05-04 23:38:52 +0800

    严林博 20230504  StringUtils.java 工具类编写

---
+++ tuling-single-mall/tuling-common/src/main/java/com/tulingxueyuan/mall/common/constant/Constants.java
@@ -0,0 +1,13 @@
+package com.tulingxueyuan.mall.common.constant;
+
+public class Constants {
+    /**
+     * http请求
+     */
+    public static final String HTTP = "http://";
+
+    /**
+     * https请求
+     */
+    public static final String HTTPS = "https://";
+}
